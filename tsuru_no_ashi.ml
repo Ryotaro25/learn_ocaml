b1bcfe47c5b6b7b8618b537ba57d4f186b6eba2b
ryotaro kurita <ryo.kuri2525@gmail.com>
2021-05-16 00:34:09 +0900

    finish turu_no_ashi

---
+++ tsuru_no_ashi.ml
@@ -0,0 +1,9 @@
+(* 目的:鶴の数xに足の本数を計算する*)
+(* tsuru_no_ashi: int -> int *)
+let tsuru_no_ashi x = x * 2
+
+(*テスト *)
+let test1 = tsuru_no_ashi 2 = 4
+let test2 = tsuru_no_ashi 20 = 40
+let test3 = tsuru_no_ashi 15 = 30
+
