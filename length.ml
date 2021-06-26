a363b04474654d403eba9f12f456428c0b39fc7d
ryotaro kurita <ryo.kuri2525@gmail.com>
2021-06-26 21:22:16 +0900

    modify length

--- length.ml
+++ length.ml
@@ -1,13 +1,16 @@
+#use "fold_right.ml"
 (* int listは
   -[] 空リスト、あるいは
   -first :: rest 最初の要素がfirstで残りのリストがrest
     という形 *)
 
+(* 目的:firstは無視してrest_resultに1を加える *)
+(* add_one : int -> int -> int*)
+let add_one first rest_result = 1 + rest_result
+
 (* 目的:受け取ったリストlstの長さを返す *)
 (* length : int list -> string *)
-let rec length lst = match lst with 
-  [] -> 0
-  | first::rest -> 1 + length rest
+let length lst = fold_right add_one lst 0
 
 (*　テスト *)
 let test1 = length [] = 0
